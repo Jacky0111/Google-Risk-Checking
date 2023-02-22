a39af9ca1d8c35cb4df1a39487c4bd3e1cf9b24e
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2023-02-22 14:31:38 +0800

    Update Google.py

--- Google.py
+++ Google.py
@@ -16,54 +16,45 @@ def main():
     df_ori = pd.read_excel(r'test-file.xlsx', engine='openpyxl')
 
     # Extract english name from Hit Name
-    df_ori['EN_HIT_NAME'] = df_ori['Hit Name'].str.replace(r'[^a-zA-Z\s]', '').str.strip()
+    df_ori['EN_HIT_NAME'] = df_ori['Hit Name'].str.replace(r'[^a-zA-Z\s]', '', regex=True).str.strip()
 
     # Combine google search link with extracted english name
     df_ori['URL'] = 'https://www.google.com/search?q=' + df_ori['EN_HIT_NAME'].str.replace(' ', '+')
 
     for index1, row in df_ori.iterrows():
-        alert_id = row[1]
-        hit_name = row[2]
-        country = row[3]
-        entry_cat = row[4]
-        entry_sub_cat = row[5]
-        ent_id = row[6]
+        alert_id, hit_name, country, entry_cat, entry_sub_cat, ent_id, url = row[1:8]
 
-        driver = setDriver()
-        driver.get(row['URL'])
-        sleep(5)
+        with setDriver() as driver:
+            driver.get(url)
+            sleep(5)
 
-        search_results = driver.find_elements(By.XPATH, "//div[contains(@class, 'yuRUbf')]/a")
+            search_results = driver.find_elements(By.XPATH, "//div[contains(@class, 'yuRUbf')]/a")
 
-        # Print the search result links
-        for index2, link in enumerate(search_results):
-            url = link.get_attribute('href')
+            # Create a list of dictionaries for each row in the search results
+            table_items_list = [{'Alert ID': alert_id,
+                                 'Hit Name': hit_name,
+                                 'Country': country,
+                                 'Entry-category': entry_cat,
+                                 'Entry-subcategory': entry_sub_cat,
+                                 'Ent Id': ent_id,
+                                 'URL': link.get_attribute('href')
+                                 } for link in search_results]
 
-            table_items = {'Alert ID': alert_id,
-                           'Hit Name': hit_name,
-                           'Country': country,
-                           'Entry-category': entry_cat,
-                           'Entry-subcategory': entry_sub_cat,
-                           'Ent Id': ent_id,
-                           'URL': url
-                           }
+            print(table_items_list)
 
-            print(index2)
-            print('----------')
-            print(table_items)
+            # Append the list of dictionaries to the final dataframe
+            df = pd.concat([df, pd.DataFrame(table_items_list)])
 
-            df = pd.concat([df, pd.DataFrame([table_items])])
-
-        # Close the browser window
-        driver.close()
-        driver.quit()
+            # Close the browser window
+            driver.close()
+            driver.quit()
 
     df.to_csv('output_file.csv', index=False, encoding='utf-8')
 
 
 def setDriver():
     option = Options()
-    option.add_argument('--headless')
+    # option.add_argument('--headless')
     option.add_argument('--disable-gpu')
     option.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
     browser = webdriver.Chrome(options=option)
