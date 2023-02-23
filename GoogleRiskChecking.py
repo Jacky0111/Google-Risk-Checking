e04bdc1729c60dccbf5cff4b2335ca76a4408c48
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2023-02-24 07:09:54 +0800

    Update GoogleRiskChecking.py

--- GoogleRiskChecking.py
+++ GoogleRiskChecking.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 from time import sleep
 from pathlib import Path
@@ -8,21 +9,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
-from VIPS.Vips import Vips
+from Output import Output
+from DOM.DomNode import DomNode
 from VIPS.VisualBlockExtraction import VisualBlockExtraction
 
 
 class GRC:
+    PDoC = 6  # Permitted Degree of Coherence
+    url = None
+    output = None
     file_name = None
     input_file = None
     output_file = None
+    window_width = None
+    window_height = None
     current_date_time = None
-    keywords = list()
+
+    keywords = []
+    node_list = []  # To store dom tree
     df1 = pd.DataFrame()
     df2 = pd.DataFrame()
     df_ori = pd.DataFrame()
 
     def __init__(self, input_file):
+        self.node_list.clear()
         self.input_file = input_file
         self.output_file = f"{Path(self.input_file).stem.replace(' ', '')}_OutputFile.xlsx"
         self.current_date_time = str(datetime.now().strftime("%H%M-%d-%b-%Y"))
@@ -37,6 +47,7 @@ class GRC:
         # Step 2: Screenshot the entire website and check the content of website with name and keywords provided.
         self.readExcelCSV(self.output_file)
         self.specificNameWebsite()
+        self.Vips()
 
     def readExcelCSV(self, file):
         try:
@@ -60,10 +71,12 @@ class GRC:
                              ' AND (~crime OR ~corruption OR ~money laundering OR ~bribe)'.replace(' ', '+')
 
     def googleSearchHitName(self):
+        count = 0
         with GRC.setDriver() as driver:
             for index, row in self.df_ori.iterrows():
                 table_items = GRC.extractHitNameResults(driver, row)
-                table_items.index = table_items.index + 1
+                table_items.index += 1 + count
+                count += table_items.shape[0]
                 table_items.index.name = 'No.'
                 self.df1 = pd.concat([self.df1, table_items])
 
@@ -71,38 +84,48 @@ class GRC:
 
     def specificNameWebsite(self):
         with GRC.setDriver() as driver:
+            self.df2['Node List'] = ''
             for index, row in self.df2.iterrows():
                 self.file_name = 'C:/Screenshots/' + str(datetime.now().strftime("%H%M-%d-%b-%Y")) + '_' + \
-                                 row['Alert ID'] + '_' + row['No.'] + '.jpeg'
+                                 str(row['Alert ID']) + '_' + str(row['No.']) + '.jpeg'
+                print('11111111111111111111111')
+                self.getJavaScript(driver, row['URL'])
+                print('22222222222222222222222')
+                # self.df2.at[index, 'Node List'] = self.node_list
+
+        # self.df2.to_csv('see.csv', index=False)
 
     '''
     Retrieve Java Script from the web page
     '''
-    def getJavaScript(self):
-        self.browser.get(self.url)
+    def getJavaScript(self, driver, url):
+        print('333333333333333333333333333333333333333')
+        driver.get(url)
+        print('open')
         sleep(5)
 
-        # Before closing the web server make sure get all the information required
-        self.window_width = 1920
-        self.window_height = self.browser.execute_script('return document.body.parentNode.scrollHeight')
-
-        self.output = Output()
-        self.output.screenshotImage(self.browser, self.window_width, self.window_height, self.file_name)
-
-        # Read in DOM java script file as string
-        file = open('DOM/dom.js', 'r')
-        java_script = file.read()
-
-        # Add additional javascript code to run our dom.js to JSON method
-        java_script += '\nreturn JSON.stringify(toJSON(document.getElementsByTagName("BODY")[0]));'
-
-        # Run the javascript
-        x = self.browser.execute_script(java_script)
-
-        self.browser.close()
-        self.browser.quit()
-
-        self.convertToDomTree(x)
+        # # Before closing the web server make sure get all the information required
+        # self.window_width = 1920
+        # self.window_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+        #
+        # self.output = Output()
+        # self.output.screenshotImage(driver, self.window_width, self.window_height, self.file_name)
+        #
+        # # Read in DOM java script file as string
+        # file = open('DOM/dom.js', 'r')
+        # java_script = file.read()
+        #
+        # # Add additional javascript code to run our dom.js to JSON method
+        # java_script += '\nreturn JSON.stringify(toJSON(document.getElementsByTagName("BODY")[0]));'
+        #
+        # # Run the javascript
+        # x = driver.execute_script(java_script)
+        #
+        driver.close()
+        # driver.quit()
+        print('close')
+        #
+        # self.convertToDomTree(x)
 
     '''
     Use the JavaScript obtained from getJavaScript() to convert to DOM Tree (Recursive Function)
@@ -110,7 +133,7 @@ class GRC:
     @param parentNode 
      @return node
     '''
-    def convertToDomTree(self, obj, parentNode=None):
+    def convertToDomTree(self, obj, parent_node=None):
         if isinstance(obj, str):
             # Use json lib to load our json string
             json_obj = json.loads(obj)
@@ -130,7 +153,7 @@ class GRC:
                 node.setVisualCues(visual_cues)
         # Text Node (Free Text)
         elif node_type == 3:
-            node.createTextNode(json_obj['nodeValue'], parentNode)
+            node.createTextNode(json_obj['nodeValue'], parent_node)
             if node.parent_node is not None:
                 visual_cues = node.parent_node.visual_cues
                 if visual_cues is not None:
@@ -153,6 +176,17 @@ class GRC:
 
         return node
 
+    def Vips(self):
+        print('Step 1: Visual Block Extraction---------------------------------------------------------------')
+        vbe = VisualBlockExtraction()
+        block = vbe.runner(self.node_list)
+        block_list = vbe.block_list
+
+        print(f'Number of Block List: {len(block_list)}')
+        # self.output.blockOutput(block_list, self.file_name)
+        # Output.textOutput(block_list, self.url)
+
+        print('---------------------------------------------Done---------------------------------------------')
 
     @staticmethod
     def extractHitNameResults(driver, row):
@@ -178,7 +212,7 @@ class GRC:
     @staticmethod
     def setDriver():
         options = Options()
-        options.add_argument('--headless')
+        # options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
         caps = DesiredCapabilities.CHROME
