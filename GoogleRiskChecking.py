9c4a43e41dede97ec9bbd9f073ac0003c1b9e858
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2023-02-24 10:02:48 +0800

    Update GoogleRiskChecking.py

--- GoogleRiskChecking.py
+++ GoogleRiskChecking.py
@@ -1,8 +1,10 @@
+import os
 import json
 import pandas as pd
 from time import sleep
 from pathlib import Path
 from datetime import datetime
+from urllib.parse import urlparse
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,7 +20,6 @@ class GRC:
     PDoC = 6  # Permitted Degree of Coherence
     url = None
     output = None
-    file_name = None
     input_file = None
     output_file = None
     window_width = None
@@ -32,7 +33,6 @@ class GRC:
     df_ori = pd.DataFrame()
 
     def __init__(self, input_file):
-        self.node_list.clear()
         self.input_file = input_file
         self.output_file = f"{Path(self.input_file).stem.replace(' ', '')}_OutputFile.xlsx"
         self.current_date_time = str(datetime.now().strftime("%H%M-%d-%b-%Y"))
@@ -45,11 +45,10 @@ class GRC:
         # self.googleSearchHitName()
 
         # Step 2: Screenshot the entire website and check the content of website with name and keywords provided.
-        self.readExcelCSV(self.output_file)
+        self.readExcel(self.output_file)
         self.specificNameWebsite()
-        self.Vips()
 
-    def readExcelCSV(self, file):
+    def readExcel(self, file):
         try:
             df_dict = pd.read_excel(file, engine='openpyxl', sheet_name=['Sheet1', 'Keywords List'])
             self.df_ori = df_dict['Sheet1']
@@ -70,62 +69,78 @@ class GRC:
         self.df_ori['URL'] = 'https://www.google.com/search?q=' + self.df_ori['EN_HIT_NAME'].str.replace(' ', '+') + \
                              ' AND (~crime OR ~corruption OR ~money laundering OR ~bribe)'.replace(' ', '+')
 
+    '''
+    Boolean search the hit name by using Chrome browser
+    '''
     def googleSearchHitName(self):
         count = 0
-        with GRC.setDriver() as driver:
-            for index, row in self.df_ori.iterrows():
-                table_items = GRC.extractHitNameResults(driver, row)
-                table_items.index += 1 + count
-                count += table_items.shape[0]
-                table_items.index.name = 'No.'
-                self.df1 = pd.concat([self.df1, table_items])
+
+        for index, row in self.df_ori.iterrows():
+            driver = GRC.setDriver()
+            table_items = GRC.extractHitNameResults(driver, row)
+            table_items.index += 1 + count
+            count += table_items.shape[0]
+            table_items.index.name = 'No.'
+            self.df1 = pd.concat([self.df1, table_items])
 
         self.df1.to_excel(self.output_file)
 
     def specificNameWebsite(self):
-        with GRC.setDriver() as driver:
-            self.df2['Node List'] = ''
-            for index, row in self.df2.iterrows():
-                self.file_name = 'C:/Screenshots/' + str(datetime.now().strftime("%H%M-%d-%b-%Y")) + '_' + \
-                                 str(row['Alert ID']) + '_' + str(row['No.']) + '.jpeg'
-                print('11111111111111111111111')
-                self.getJavaScript(driver, row['URL'])
-                print('22222222222222222222222')
-                # self.df2.at[index, 'Node List'] = self.node_list
+        self.df2['Node List'] = ''
 
-        # self.df2.to_csv('see.csv', index=False)
+        for index, row in self.df2.iterrows():
+            self.node_list.clear()
+            driver = GRC.setDriver()
+            date_time = datetime.now().strftime("%H%M-%d-%b-%Y")
+            user_file_path = GRC.setUserReferenceFileName(str(date_time), str(row['Alert ID']), str(row['No.']))
+            dev_file_path = GRC.setFolderName(row['URL'], str(date_time), str(row['Alert ID']), str(row['No.']))
+            path_list = [user_file_path, dev_file_path]
+            self.getJavaScript(driver, row['URL'], path_list)
+            self.Vips(path_list)
+            break
+
+    def Vips(self, path_list):
+        print('Step 1: Visual Block Extraction---------------------------------------------------------------')
+        vbe = VisualBlockExtraction()
+        block = vbe.runner(self.node_list)
+        block_list = vbe.block_list
+
+        print(f'Number of Block List: {len(block_list)}')
+        self.output.blockOutput(block_list, path_list[1])
+        # Output.textOutput(block_list, self.url)
+
+        print('---------------------------------------------Done---------------------------------------------')
+
 
     '''
     Retrieve Java Script from the web page
     '''
-    def getJavaScript(self, driver, url):
-        print('333333333333333333333333333333333333333')
+    def getJavaScript(self, driver, url, path_list):
         driver.get(url)
-        print('open')
         sleep(5)
 
-        # # Before closing the web server make sure get all the information required
-        # self.window_width = 1920
-        # self.window_height = driver.execute_script('return document.body.parentNode.scrollHeight')
-        #
-        # self.output = Output()
-        # self.output.screenshotImage(driver, self.window_width, self.window_height, self.file_name)
-        #
-        # # Read in DOM java script file as string
-        # file = open('DOM/dom.js', 'r')
-        # java_script = file.read()
-        #
-        # # Add additional javascript code to run our dom.js to JSON method
-        # java_script += '\nreturn JSON.stringify(toJSON(document.getElementsByTagName("BODY")[0]));'
-        #
-        # # Run the javascript
-        # x = driver.execute_script(java_script)
-        #
+        # Before closing the web server make sure get all the information required
+        self.window_width = 1920
+        self.window_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+
+        self.output = Output()
+        self.output.screenshotImage(driver, self.window_width, self.window_height, path_list)
+
+        # Read in DOM java script file as string
+        file = open('DOM/dom.js', 'r')
+        java_script = file.read()
+
+        # Add additional javascript code to run our dom.js to JSON method
+        java_script += '\nreturn JSON.stringify(toJSON(document.getElementsByTagName("BODY")[0]));'
+
+        # Run the javascript
+        x = driver.execute_script(java_script)
+
         driver.close()
-        # driver.quit()
+        driver.quit()
         print('close')
-        #
-        # self.convertToDomTree(x)
+
+        self.convertToDomTree(x)
 
     '''
     Use the JavaScript obtained from getJavaScript() to convert to DOM Tree (Recursive Function)
@@ -176,17 +191,7 @@ class GRC:
 
         return node
 
-    def Vips(self):
-        print('Step 1: Visual Block Extraction---------------------------------------------------------------')
-        vbe = VisualBlockExtraction()
-        block = vbe.runner(self.node_list)
-        block_list = vbe.block_list
 
-        print(f'Number of Block List: {len(block_list)}')
-        # self.output.blockOutput(block_list, self.file_name)
-        # Output.textOutput(block_list, self.url)
-
-        print('---------------------------------------------Done---------------------------------------------')
 
     @staticmethod
     def extractHitNameResults(driver, row):
@@ -207,17 +212,37 @@ class GRC:
                              'URL': link.get_attribute('href')
                              } for link in search_results]
 
+        driver.close()
+        driver.quit()
+
         return pd.DataFrame(table_items_list)
 
     @staticmethod
     def setDriver():
         options = Options()
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
         caps = DesiredCapabilities.CHROME
         browser = webdriver.Chrome(desired_capabilities=caps, options=options)
         return browser
+
+    '''
+    Create a folder at C drive where every use can access from their pc and save screenshot to the folder
+    '''
+    @staticmethod
+    def setUserReferenceFileName(dt, alert_id, number):
+        return 'C:/Screenshots/' + dt + '_' + alert_id + '_' + number
+
+    '''
+    Set the folder name and make directory
+    '''
+    @staticmethod
+    def setFolderName(url, dt, alert_id, number):
+        parse_url = urlparse(url)
+        path = r'Screenshots/' + dt + '_' + alert_id + '_' + number + '/'
+        os.makedirs(path)
+        return path + parse_url.netloc
 
 
 if __name__ == '__main__':
