044527b9324ccdee7ee07df589d8cef66eb5dfcc
Chia Chung <limchiachung0611@gmail.com>
2023-03-07 11:08:15 +0800

    Update name.py

--- name.py
+++ name.py
@@ -1,45 +1,16 @@
 import spacy
-from fuzzywuzzy import fuzz
-import itertools
 
+# Load the small English model
 nlp = spacy.load('en_core_web_sm')
 
-essay = "Jn Smth was born in New York City. He studied at Harvard University and later became a successful businessman."
+# Define a sentence to analyze
+sentence = '''Money laundering is a process of converting cash, funds or property derived from criminal activities to give it a legitimate appearance. It is a process to clean ‘dirty’ money in order to disguise its criminal origin.
 
-# Set the person name you want to detect as a keyword
-person_name = "John Smith"
+'''
+# Process the sentence with spaCy
+doc = nlp(sentence)
 
-# Process the essay text using spaCy
-doc = nlp(essay)
-
-# Split the person name into individual tokens
-person_name_tokens = person_name.split()
-
-# Find all permutations of the person name
-person_name_permutations = [
-    " ".join(permutation)
-    for r in range(1, len(person_name_tokens) + 1)
-    for permutation in itertools.permutations(person_name_tokens, r)
-]
-
-print(person_name_permutations)
-
-# Iterate over the words and phrases in the document
-for i, token in enumerate(doc):
-    # Check if the token matches the first token of any permutation of the person name
-    for permutation in person_name_permutations:
-        # Check if the remaining tokens match the subsequent tokens of the permutation
-        j = 0
-        while i + j < len(doc) and j < len(permutation.split()):
-            distance = fuzz.ratio(doc[i + j].text.lower(), permutation.split()[j].lower())
-            if distance < 65:
-                break
-            j += 1
-        # If all tokens match, print the match and break the loop to avoid duplicate matches
-        if j == len(permutation.split()):
-            print(f"Detected person name: {permutation}")
-            print("Matched tokens:")
-            for k in range(i, i + len(permutation.split())):
-                print(doc[k].text)
-            print("\n")
-            break
+# Iterate over the entities in the sentence and check if any of them are a person
+for entity in doc.ents:
+    if entity.label_ == 'PERSON':
+        print(entity)
